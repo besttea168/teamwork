de8f19bb4143171524a7cfa01818f051668edf17
JZ <sam6032001@gmail.com>
2024-08-20 10:05:45 +0800

    更新siderbar選單

--- sidebar.php
+++ sidebar.php
@@ -2,11 +2,11 @@
     <div class="accordion accordion-flush" id="accordionFlushExample">
         <div class="accordion-item">
             <h2 class="accordion-header" id="flush-headingUser">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseUser" aria-expanded="false" aria-controls="flush-collapseUser">
                     會員管理
                 </button>
             </h2>
-            <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingUser" data-bs-parent="#accordionFlushExample">
+            <div id="flush-collapseUser" class="accordion-collapse collapse" aria-labelledby="flush-headingUser" data-bs-parent="#accordionFlushExample">
                 <div class="accordion-body bg-dark">
                     <div class="list-group text-center">
                         <a href="#" class="list-group-item list-group-item-action">會員列表</a>
@@ -28,7 +28,22 @@
                     <div class="list-group text-center">
                         <a href="#" class="list-group-item list-group-item-action">商品列表</a>
                         <a href="#" class="list-group-item list-group-item-action">新增商品</a>
-                        <a href="#" class="list-group-item list-group-item-action">編輯商品</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingRent">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseRent" aria-expanded="false" aria-controls="flush-collapseRent">
+                    租借管理
+                </button>
+            </h2>
+            <div id="flush-collapseRent" class="accordion-collapse collapse" aria-labelledby="flush-headingRent" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body bg-dark">
+                    <div class="list-group text-center">
+                        <a href="#" class="list-group-item list-group-item-action">租借列表</a>
+                        <a href="#" class="list-group-item list-group-item-action">新增商品</a>
                     </div>
                 </div>
             </div>
@@ -49,8 +64,30 @@
                 </div>
             </div>
         </div>
+
+
+
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingArticle">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseArticle" aria-expanded="false" aria-controls="flush-collapseArticle">
+                    文章管理
+                </button>
+            </h2>
+            <div id="flush-collapseArticle" class="accordion-collapse collapse" aria-labelledby="flush-headingArticle" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body bg-dark">
+                    <div class="list-group text-center">
+                        <a href="" class="list-group-item list-group-item-action">文章列表</a>
+                        <a href="" class="list-group-item list-group-item-action">新增文章</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+
     </div>
 </aside>
+
 
 
 <!-- Bootstrap JavaScript Libraries 功能有用到不能刪-->
