6800b50f0de50caabedfded53519fef9c8829031
Shawn <liumingshuny@gmail.com>
2024-08-20 11:40:39 +0800

    系統頁面連動，新增style.php , header.php 跟新ui

--- article/artical _manage_system.php
+++ article/artical _manage_system.php
@@ -8,96 +8,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
     <!-- Bootstrap CSS v5.2.1 -->
-  <?php include("../css.php") ?>
-    <style>
-        .function-box {
-            height: 50px;
-            width: 180px;
-            border: 2px solid darkgray;
-            border-radius: 15px;
-
-            
-        }
-    </style>
+    <?php include("../css.php") ?>
+    <?php include("style.php") ?>
 </head>
 
 <body>
+    <?php include("../article/header.php") ?>
 
-    <header class="main-header d-flex justify-content-between align-items-center fixed-top shadow">
-        <a href="" class="brand text-decoration-none link-light p-3 bg-dark">桌遊仔</a>
-        <a href="" class="btn btn-dark">系統登出</a>
-    </header>
-
-    <aside class="left-aside bg-light border-end vh-100 position-fixed top-0 start-0 overflow-auto">
-        <ul class="list-unstyled">
-            <li>
-                <a href="" class="d-block px-3 py-2 text-decoration-none"><i class="fa-solid fa-house me-2"></i>
-                    系統首頁</a>
-
-            </li>
-            <li>
-                <a href="" class="d-block px-3 py-2 text-decoration-none"><i class="fa-solid fa-file-pen me-2"></i>
-                    建立新文章</a>
-            </li>
-            <li>
-                <a href="" class="d-block px-3 py-2 text-decoration-none"><i
-                        class="fa-solid fa-magnifying-glass me-2"></i>
-                    搜尋現有文章</a>
-            </li>
-            <li>
-                <a href="" class="d-block px-3 py-2 text-decoration-none"><i class="fa-solid fa-book-bookmark me-2"></i>
-                    按分類檢索文章</a>
-            </li>
-            <li>
-                <a href="" class="d-block px-3 py-2 text-decoration-none text-muted"><i
-                        class="fa-solid fa-trash-can me-2"></i>
-                    刪除文章(暫時不做)</a><!----暫時不做-->
-            </li>
-            <li>
-                <a href="" class="d-block px-3 py-2 text-decoration-none"><i
-                        class="fa-solid fa-puzzle-piece me-2"></i></i>
-                    Intergration</a>
-            </li>
-
-        </ul>
-        <div class="d-flex justify-content-between px-3 align-items-center ">
-            <small class="text-muted">SAVED REPORT</small>
-            <a role="button"><i class="fa-solid fa-circle-plus"></i></a>
-        </div>
-        <ul class="list-unstyled">
-            <li>
-                <a href="" class="d-block px-3 py-2 text-decoration-none"><i class="fa-regular fa-newspaper me-2"></i>
-                    Current Month</a>
-
-            </li>
-            <li>
-                <a href="" class="d-block px-3 py-2 text-decoration-none"><i class="fa-regular fa-newspaper me-2"></i>
-                    Last Quater</a>
-
-            </li>
-            <li>
-                <a href="" class="d-block px-3 py-2 text-decoration-none"><i class="fa-regular fa-newspaper me-2"></i>
-                    Social engagement</a>
-
-            </li>
-            <li>
-                <a href="" class="d-block px-3 py-2 text-decoration-none"><i class="fa-regular fa-newspaper me-2"></i>
-                    Year-end sale</a>
-            </li>
-        </ul>
-        <hr>
-        <ul class="list-unstyled">
-            <li>
-                <a href="" class="d-block px-3 py-2 text-decoration-none"><i class="fa-solid fa-user-gear me-2"></i>
-                    系統設定</a>
-            </li>
-            <li>
-                <a href="" class="d-block px-3 py-2 text-decoration-none"><i
-                        class="fa-solid fa-right-from-bracket me-2"></i>
-                    系統登出</a>
-            </li>
-        </ul>
-    </aside>
     <main class="main-content pp-3 px-3">
         <div class="d-flex justify-content-between">
             <h1>功能頁面總覽</h1>
@@ -118,30 +35,27 @@
         </div>
         <hr>
         <div class="container">
-            <ul class="list-unstyled row">
+            <ul class="list-unstyled ">
                 <li class="col">
                     <div class="function-box pt-2 ps-4">
-                        <a href="">建立新文章</a>
+                        <a href="../article/createNewArticle.php"><i class="fa-solid fa-pen-nib"></i>建立新文章</a>
                     </div>
                 </li>
                 <li class="col">
                     <div class="function-box pt-2 ps-4">
-                        <a href="">編輯瀏覽文章</a>
+                        <a href="../article/mainArticle.html">文章總覽</a>
                     </div>
                 </li>
                 <li class="col">
                     <div class="function-box pt-2 ps-4">
-                        <a href="">文章搜尋（按標題）</a>
+                        <a href="../article/SearchArticle.html">文章搜尋</a>
                     </div>
+                </li>
+
                 </li>
                 <li class="col">
                     <div class="function-box pt-2 ps-4">
-                        <a href="">文章搜尋（按時間）</a>
-                    </div>
-                </li>
-                <li class="col">
-                    <div class="function-box pt-2 ps-4">
-                        <a href="">文章分類總覽</a>
+                        <a href="../article/categroyArticle.html">文章分類總覽</a>
                     </div>
                 </li>
             </ul>
--- article/style.php
+++ article/style.php
@@ -1,29 +1,15 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <style>
-        :root {
-            --aside-width: 240px;
-            --top-width: 72px;
+<style>
+    .function-box {
+        height: 100px;
+        width: 100%;
+        border: 2px solid darkgray;
+        border-radius: 15px;
+        
+        align-items: center;
+        a{
+            font-size: 48px;
+            display: block;
+            text-decoration: none;
         }
-
-        .main-header {
-
-            background: #00000091;
-        }
-
-        .brand {
-            width: var(--aside-width);
-        }
-
-        .left-aside {
-            width: var(--aside-width);
-            padding-top: var(--top-width);
-        }
-
-        .main-content {
-            margin: var(--top-width) 0 0 var(--aside-width);
-        }
-    </style>
+    }
+</style>
