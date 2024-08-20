fca7590cb728547b8d916fcbb63ef22a073a8c2c
Muji <08130361@me.mcu.edu.tw>
2024-08-20 12:47:26 +0800

    新增租借商品列表

--- db_connect.php
+++ db_connect.php
@@ -10,7 +10,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   	die("連線失敗: " . $conn->connect_error);
 }else{
-    // echo "OK!";
+    echo "連線成功";
 }
 
 ?>
