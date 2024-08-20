3da8d5792e06b6363cd0017a097577094586512f
JZ <sam6032001@gmail.com>
2024-08-20 11:58:06 +0800

    T

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $username = "admin";
-$password = "12345";
-$dbname = "teamwork";
+$password = "admin";
+$dbname = "mfee57";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
