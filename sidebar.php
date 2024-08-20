2e5c1b6192f19258ebca1996df5494d5f1351b63
JZ <sam6032001@gmail.com>
2024-08-20 09:27:53 +0800

    修改連結、小部分版面

--- sidebar.php
+++ sidebar.php
@@ -1,59 +1,59 @@
 <aside class="left-aside bg-dark border-end border-secondary vh-100 position-fixed top-0 start-0 overflow-auto">
-        <div class="accordion accordion-flush" id="accordionFlushExample">
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="flush-headingUser">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                        會員管理
-                    </button>
-                </h2>
-                <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingUser" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body bg-dark">
-                        <div class="list-group text-center">
-                            <a href="#" class="list-group-item list-group-item-action">會員列表</a>
-                            <a href="#" class="list-group-item list-group-item-action">會員</a>
-                            <a href="#" class="list-group-item list-group-item-action">會員</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="flush-headingProduct">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseProduct" aria-expanded="false" aria-controls="flush-collapseProduct">
-                        商品管理
-                    </button>
-                </h2>
-                <div id="flush-collapseProduct" class="accordion-collapse collapse" aria-labelledby="flush-headingProduct" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body bg-dark">
-                        <div class="list-group text-center">
-                            <a href="#" class="list-group-item list-group-item-action">商品列表</a>
-                            <a href="#" class="list-group-item list-group-item-action">新增商品</a>
-                            <a href="#" class="list-group-item list-group-item-action">編輯商品</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="flush-headingCoupon">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseCoupon" aria-expanded="false" aria-controls="flush-collapseCoupon">
-                        優惠券管理
-                    </button>
-                </h2>
-                <div id="flush-collapseCoupon" class="accordion-collapse collapse" aria-labelledby="flush-headingCoupon" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body bg-dark">
-                        <div class="list-group text-center">
-                            <a href="../teamwork1/coupon/coupons.php" class="list-group-item list-group-item-action">優惠券列表</a>
-                            <a href="../teamwork1/coupon/create-coupon.php" class="list-group-item list-group-item-action">新增優惠券</a>
-                        </div>
+    <div class="accordion accordion-flush" id="accordionFlushExample">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingUser">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                    會員管理
+                </button>
+            </h2>
+            <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingUser" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body bg-dark">
+                    <div class="list-group text-center">
+                        <a href="#" class="list-group-item list-group-item-action">會員列表</a>
+                        <a href="#" class="list-group-item list-group-item-action">會員</a>
+                        <a href="#" class="list-group-item list-group-item-action">會員</a>
                     </div>
                 </div>
             </div>
         </div>
+
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingProduct">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseProduct" aria-expanded="false" aria-controls="flush-collapseProduct">
+                    商品管理
+                </button>
+            </h2>
+            <div id="flush-collapseProduct" class="accordion-collapse collapse" aria-labelledby="flush-headingProduct" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body bg-dark">
+                    <div class="list-group text-center">
+                        <a href="#" class="list-group-item list-group-item-action">商品列表</a>
+                        <a href="#" class="list-group-item list-group-item-action">新增商品</a>
+                        <a href="#" class="list-group-item list-group-item-action">編輯商品</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingCoupon">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseCoupon" aria-expanded="false" aria-controls="flush-collapseCoupon">
+                    優惠券管理
+                </button>
+            </h2>
+            <div id="flush-collapseCoupon" class="accordion-collapse collapse" aria-labelledby="flush-headingCoupon" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body bg-dark">
+                    <div class="list-group text-center">
+                        <a href="../coupon/coupons.php" class="list-group-item list-group-item-action">優惠券列表</a>
+                        <a href="../coupon/create-coupon.php" class="list-group-item list-group-item-action">新增優惠券</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </aside>
 
 
-    <!-- Bootstrap JavaScript Libraries 功能有用到不能刪-->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<!-- Bootstrap JavaScript Libraries 功能有用到不能刪-->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
