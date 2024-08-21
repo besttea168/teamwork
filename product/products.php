01d4f1f7702cd9c14894f62a98235c779c99b55f
Kenja <besttea0800@gmail.com>
2024-08-21 19:14:31 +0800

    產品列表暫完成

--- product/products.php
+++ product/products.php
@@ -30,13 +30,13 @@ if (isset($_GET["search"])) {
             break;
 
         case 3:
-            $where_clause = "ORDER BY name ASC";
+            $where_clause = "ORDER BY price ASC";
             break;
 
         case 4:
-            $where_clause = "ORDER BY name DESC";
+            $where_clause = "ORDER BY price DESC";
             break;
-        
+
         default:
             header("Location: products.php?p=1&order=1");
     }
@@ -70,7 +70,7 @@ if (isset($_GET["search"])) {
 
     <?php include("../css.php") ?>
     <style>
-        img{
+        img {
             width: 100px;
             height: 100px;
         }
@@ -84,7 +84,9 @@ if (isset($_GET["search"])) {
             <?php if (isset($_GET["search"])) : ?>
                 <a class="btn btn-primary" href="products.php" title="回商品列表"><i class="fa-solid fa-arrow-left"></i></a>
             <?php endif; ?>
-            <a class="btn btn-primary" href="create-product.php"><i class="fa-solid fa-square-plus"></i></a>
+            <?php if (!isset($_GET["search"])) : ?>
+                <a class="btn btn-primary" href="create-product.php"><i class="fa-solid fa-square-plus"></i></a>
+            <?php endif; ?>
         </div>
 
         <div class="py-2">
@@ -99,10 +101,10 @@ if (isset($_GET["search"])) {
         <?php if (isset($_GET["p"])) : ?>
             <div class="py-2 d-flex justify-content-end">
                 <div class="btn-group">
-                    <a class="btn btn-primary <?php if ($order == 1) echo "active" ?>" href="products.php?p=<?= $page ?>&order=1"><i class="fa-solid fa-arrow-down-1-9"></i></a>
-                    <a class="btn btn-primary <?php if ($order == 2) echo "active" ?>" href="products.php?p=<?= $page ?>&order=2"><i class="fa-solid fa-arrow-down-9-1"></i></a>
-                    <a class="btn btn-primary <?php if ($order == 3) echo "active" ?>" href="products.php?p=<?= $page ?>&order=3"><i class="fa-solid fa-arrow-down-a-z"></i></a>
-                    <a class="btn btn-primary <?php if ($order == 4) echo "active" ?>" href="products.php?p=<?= $page ?>&order=4"><i class="fa-solid fa-arrow-down-z-a"></i></a>
+                    <a title="id從小到大" class="btn btn-primary <?php if ($order == 1) echo "active" ?>" href="products.php?p=<?= $page ?>&order=1"><i class="fa-solid fa-arrow-down-1-9"></i></a>
+                    <a title="id從大到小" class="btn btn-primary <?php if ($order == 2) echo "active" ?>" href="products.php?p=<?= $page ?>&order=2"><i class="fa-solid fa-arrow-down-9-1"></i></a>
+                    <a title="price從小到大" class="btn btn-primary <?php if ($order == 3) echo "active" ?>" href="products.php?p=<?= $page ?>&order=3"><i class="fa-solid fa-dollar-sign"></i><i class="fa-solid fa-up-long"></i></a>
+                    <a title="price從大到小" class="btn btn-primary <?php if ($order == 4) echo "active" ?>" href="products.php?p=<?= $page ?>&order=4"><i class="fa-solid fa-dollar-sign"></i><i class="fa-solid fa-down-long"></i></a>
                 </div>
             </div>
         <?php endif; ?>
@@ -129,11 +131,13 @@ if (isset($_GET["search"])) {
                             <td><?= $product["id"] ?></td>
                             <td><?= $product["name"] ?></td>
                             <td><img class="object-fit-cover" src="../product_img/<?= urlencode($product["image"]) ?>" alt="<?= $product["name"] ?>"></td>
+                            <!-- 使用 PHP 的 urlencode 函數對圖片名稱進行編碼。這樣可以確保圖片名稱中的特殊字符被正確處理。 -->
                             <td><?= $product["category_tag"] ?></td>
                             <td><?= $product["price"] ?></td>
                             <td><?= $product["created_at"] ?></td>
                             <td>
                                 <a class="btn btn-primary" href="product.php?id=<?= $product["id"] ?>"><i class="fa-solid fa-eye"></i></a>
+                                <a class="btn btn-primary" href="product-edit.php?id=<?= $product["id"]?>"><i class="fa-solid fa-pen-to-square"></i></a>
                             </td>
                         </tr>
                     <?php endforeach ?>
@@ -142,9 +146,31 @@ if (isset($_GET["search"])) {
             <?php if (isset($_GET["p"])) : ?>
                 <nav aria-label="Page navigation example">
                     <ul class="pagination">
-                        <?php for ($i = 1; $i <= $total_page; $i++) : ?>
+                        <?php
+                        $visible_pages = 5; // Number of visible pagination links
+                        $start_page = max(1, $page - floor($visible_pages / 2));
+                        $end_page = min($total_page, $start_page + $visible_pages - 1);
+
+                        if ($start_page > 1) {
+                            echo '<li class="page-item"><a class="page-link" href="products.php?p=1&order=' . $order . '"> << </a></li>';
+                        }
+
+                        if ($page > 1) {
+                            echo '<li class="page-item"><a class="page-link" href="products.php?p=' . ($page - 1) . '&order=' . $order . '"> < </a></li>';
+                        }
+
+                        for ($i = $start_page; $i <= $end_page; $i++) : ?>
                             <li class="page-item <?php if ($page == $i) echo "active"; ?>"><a class="page-link" href="products.php?p=<?= $i ?>&order=<?= $order ?>"><?= $i ?></a></li>
-                        <?php endfor ?>
+                        <?php endfor;
+
+                        if ($page < $total_page) {
+                            echo '<li class="page-item"><a class="page-link" href="products.php?p=' . ($page + 1) . '&order=' . $order . '"> > </a></li>';
+                        }
+
+                        if ($end_page < $total_page) {
+                            echo '<li class="page-item"><a class="page-link" href="products.php?p=' . $total_page . '&order=' . $order . '"> >> </a></li>';
+                        }
+                        ?>
                     </ul>
                 </nav>
             <?php endif; ?>
