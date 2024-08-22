09d64c210fdc9b46b2e3df5fc6a33c0cf1b35ead
Shawn <liumingshuny@gmail.com>
2024-08-22 15:45:23 +0800

    TESTING SHIT

--- article/doCreateArticle.php
+++ article/doCreateArticle.php
@@ -1,5 +1,18 @@
 <?php
-require_once("../db_connect.php");
+require_once("../article/db_connect.php");
+$title = "testtitle";
+$content = "testcontent";
+$image_id = "testimgig";
+$description = "testdes";
+$category_id = "testcat";
+$sql = "INSERT INTO article_main (title, content, image_id, 	description, category_id) 
+        VALUES ('$title','$content','$image_id','$description','$category_id') ";
 
-$sql=""
+if ($conn->query($sql) === TRUE) {
+    $last_id = $conn->insert_id;
+    echo "新文章輸入成功 id 為 $last_id";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+echo ($sql);
 ?>
--- article/quilldemo.php
+++ article/quilldemo.php
@@ -26,9 +26,7 @@
                 <p>Some initial <strong>bold</strong> text</p>
                 <p><br /></p>
             </div>
-            <button type="submit" class="btn btn-primary" id="submitBtn">
-                submit
-            </button>
+            <input type="submit" value="Save" id="submitBtn" />
 
         </div>
 
@@ -37,19 +35,31 @@
 
     <!-- Include the Quill library -->
     <script src="https://cdn.jsdelivr.net/npm/quill@2.0.2/dist/quill.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.js"
+        integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+
 
     <!-- Initialize Quill editor -->
     <script>
         const quill = new Quill('#editor', {
             theme: 'snow'
         });
-        const length = quill.getLength();
-        const text = quill.getText(0, length);
-        function getContentFromQuill(params) {
-            
-        }
-        const content =document.getElementById("submitBtn").addEventListener("click",function(){
-            console.log(text);
-        })
+
+
+        const content = document.getElementById("submitBtn").addEventListener("click", function getthings() {
+            const length = quill.getLength();
+            const text = quill.getContents();
+            const outpus = JSON.stringify(text)
+            return outpus;
+        });
+        $.ajax(
+            {
+                method: "POST",
+                url: "../article/doCreateArticle.php",
+                dataType: "json",
+            }
+        )
+            ;
+
     </script>
 </body>
