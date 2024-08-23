e16c2f7574a853d987fe2fba6399c9f464241348
Shawn <liumingshuny@gmail.com>
2024-08-23 16:09:01 +0800

    新增文章系統頁籤至公版sidebar

--- sidebar.php
+++ sidebar.php
@@ -2,11 +2,13 @@
     <div class="accordion accordion-flush" id="accordionFlushExample">
         <div class="accordion-item">
             <h2 class="accordion-header" id="flush-headingUser">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseUser" aria-expanded="false" aria-controls="flush-collapseUser">
-                <i class="fa-solid fa-user mx-3"></i>會員管理
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#flush-collapseUser" aria-expanded="false" aria-controls="flush-collapseUser">
+                    <i class="fa-solid fa-user mx-3"></i>會員管理
                 </button>
             </h2>
-            <div id="flush-collapseUser" class="accordion-collapse collapse" aria-labelledby="flush-headingUser" data-bs-parent="#accordionFlushExample">
+            <div id="flush-collapseUser" class="accordion-collapse collapse" aria-labelledby="flush-headingUser"
+                data-bs-parent="#accordionFlushExample">
                 <div class="accordion-body bg-dark">
                     <div class="list-group text-center">
                         <a href="#" class="list-group-item list-group-item-action">會員列表</a>
@@ -16,16 +18,18 @@
                 </div>
             </div>
 
-            
+
         </div>
 
         <div class="accordion-item">
             <h2 class="accordion-header" id="flush-headingProduct">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseProduct" aria-expanded="false" aria-controls="flush-collapseProduct">
-                <i class="fa-solid fa-cart-shopping mx-3"></i>商品管理
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#flush-collapseProduct" aria-expanded="false" aria-controls="flush-collapseProduct">
+                    <i class="fa-solid fa-cart-shopping mx-3"></i>商品管理
                 </button>
             </h2>
-            <div id="flush-collapseProduct" class="accordion-collapse collapse" aria-labelledby="flush-headingProduct" data-bs-parent="#accordionFlushExample">
+            <div id="flush-collapseProduct" class="accordion-collapse collapse" aria-labelledby="flush-headingProduct"
+                data-bs-parent="#accordionFlushExample">
                 <div class="accordion-body bg-dark">
                     <div class="list-group text-center">
                         <a href="../product/products.php" class="list-group-item list-group-item-action">商品列表</a>
@@ -37,15 +41,18 @@
 
         <div class="accordion-item">
             <h2 class="accordion-header" id="flush-headingRent">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseRent" aria-expanded="false" aria-controls="flush-collapseRent">
-                <i class="fa-solid fa-cart-flatbed mx-3"></i>租借管理
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#flush-collapseRent" aria-expanded="false" aria-controls="flush-collapseRent">
+                    <i class="fa-solid fa-cart-flatbed mx-3"></i>租借管理
                 </button>
             </h2>
-            <div id="flush-collapseRent" class="accordion-collapse collapse" aria-labelledby="flush-headingRent" data-bs-parent="#accordionFlushExample">
+            <div id="flush-collapseRent" class="accordion-collapse collapse" aria-labelledby="flush-headingRent"
+                data-bs-parent="#accordionFlushExample">
                 <div class="accordion-body bg-dark">
                     <div class="list-group text-center">
                         <a href="../rent/rent_product_list.php" class="list-group-item list-group-item-action">租借列表</a>
-                        <a href="../rent/create-rent-product.php" class="list-group-item list-group-item-action">新增商品</a>
+                        <a href="../rent/create-rent-product.php"
+                            class="list-group-item list-group-item-action">新增商品</a>
                     </div>
                 </div>
             </div>
@@ -53,11 +60,13 @@
 
         <div class="accordion-item">
             <h2 class="accordion-header" id="flush-headingCoupon">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseCoupon" aria-expanded="false" aria-controls="flush-collapseCoupon">
-                <i class="fa-solid fa-ticket mx-3"></i>優惠券管理
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#flush-collapseCoupon" aria-expanded="false" aria-controls="flush-collapseCoupon">
+                    <i class="fa-solid fa-ticket mx-3"></i>優惠券管理
                 </button>
             </h2>
-            <div id="flush-collapseCoupon" class="accordion-collapse collapse" aria-labelledby="flush-headingCoupon" data-bs-parent="#accordionFlushExample">
+            <div id="flush-collapseCoupon" class="accordion-collapse collapse" aria-labelledby="flush-headingCoupon"
+                data-bs-parent="#accordionFlushExample">
                 <div class="accordion-body bg-dark">
                     <div class="list-group text-center">
                         <a href="../coupon/coupons.php" class="list-group-item list-group-item-action">優惠券列表</a>
@@ -71,36 +80,46 @@
 
         <div class="accordion-item">
             <h2 class="accordion-header" id="flush-headingArticle">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseArticle" aria-expanded="false" aria-controls="flush-collapseArticle">
-                <i class="fa-solid fa-newspaper mx-3"></i>文章管理
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#flush-collapseArticle" aria-expanded="false" aria-controls="flush-collapseArticle">
+                    <i class="fa-solid fa-newspaper mx-3"></i>文章管理
                 </button>
             </h2>
-            <div id="flush-collapseArticle" class="accordion-collapse collapse" aria-labelledby="flush-headingArticle" data-bs-parent="#accordionFlushExample">
+            <div id="flush-collapseArticle" class="accordion-collapse collapse" aria-labelledby="flush-headingArticle"
+                data-bs-parent="#accordionFlushExample">
                 <div class="accordion-body bg-dark">
                     <div class="list-group text-center">
-                        <a href="" class="list-group-item list-group-item-action">文章列表</a>
-                        <a href="" class="list-group-item list-group-item-action">新增文章</a>
+                        <a href="../article/article_manage_system.php"
+                            class="list-group-item list-group-item-action">文章系統首頁</a>
+                        <a href="../article/createNewArticle.php"
+                            class="list-group-item list-group-item-action">建立新文章</a>
+                        <a href="../article/SearchArticle.php" class="list-group-item list-group-item-action">搜尋現有文章</a>
+                        <a href="../article/categoryArticle.php"
+                            class="list-group-item list-group-item-action">文章類別總覽</a>
+                        <a href="../article/mainArticle.php" class="list-group-item list-group-item-action">文章總覽</a>
                     </div>
                 </div>
             </div>
         </div>
 
         <div class="accordion-item">
-                <h2 class="accordion-header" id="flush-headingVideo">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseVideo" aria-expanded="false" aria-controls="flush-collapseVideo">
+            <h2 class="accordion-header" id="flush-headingVideo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#flush-collapseVideo" aria-expanded="false" aria-controls="flush-collapseVideo">
                     <i class="fa-solid fa-film mx-3"></i>教學影片管理
-                    </button>
-                </h2>
-                <div id="flush-collapseVideo" class="accordion-collapse collapse" aria-labelledby="flush-headingVideo" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body bg-dark">
-                        <div class="list-group text-center">
-                            <a href="#" class="list-group-item list-group-item-action">教學影片列表</a>
-                            <a href="#" class="list-group-item list-group-item-action">新增影片</a>
-                        </div>
+                </button>
+            </h2>
+            <div id="flush-collapseVideo" class="accordion-collapse collapse" aria-labelledby="flush-headingVideo"
+                data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body bg-dark">
+                    <div class="list-group text-center">
+                        <a href="#" class="list-group-item list-group-item-action">教學影片列表</a>
+                        <a href="#" class="list-group-item list-group-item-action">新增影片</a>
                     </div>
                 </div>
             </div>
-        
+        </div>
+
 
     </div>
 </aside>
@@ -108,6 +127,10 @@
 
 
 <!-- Bootstrap JavaScript Libraries 功能有用到不能刪-->
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+    crossorigin="anonymous"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+    crossorigin="anonymous"></script>
